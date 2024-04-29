937600011d6b60c65368edfc63d8d686af9d33b6
PedroPazosCurra <pedropazos11@gmail.com>
2024-04-29 14:31:52 +0200

    Contexto y formato de programa python para pruebas de prompts

--- src/procesa_mensaje.py
+++ src/procesa_mensaje.py
@@ -3,16 +3,43 @@ import requests
 import json
 
 AWANLLM_API_KEY = "cee74e0e-35c6-46b0-b4d5-071396a48e32"
-url = "https://api.awanllm.com/v1/completions"
-prompt = "You're a Natural Language to Answer Set Programming translator. Input: John is hungry. / Output: hungry(john) / Input: Mary is drinking ale / Output: drinking(mary,ale) / Input: Albert lives in a red house / Output: "
 modelo = "Meta-Llama-3-8B-Instruct-Dolfin-v0.1"
+url = "https://api.awanllm.com/v1/completions"
+
+prompt = "Blends tobacco is smoked at the fourth house."
+
+contexto = "You're a Natural Language to Answer Set Programming translator machine. You have to answer with only the result like the provided iterations and nothing more. Do not reply with a sentence, only with the output code. Don't explain the result and don't say anything else that the result.\n \
+            Input: John is hungry.\n\
+            Output: hungry(john).\n\
+            Input: The house is red.\n\
+            Output: color(red, house).\n\
+            Input: The norwegian lives at the first house.\n\
+            Output: at(person,norw,1).\n\
+            Input: The brit lives at the third house.\n\
+            Output: at(person,brit,3).\n\
+            Input: The first house has a dog pet.\n\
+            Output: at(pet, dog, 1) \n\
+            Input: The german is living at the green house.\n\
+            Output: :- at(person,german,X), not at(color, green, X)\n\
+            Input: In the purple house, they smoke Dunhill cigarettes.\n\
+            Output: :- at(color, purple, X), not at(smoke, dunhill, X).\n\
+            Input: Coffee is drunk at the red house.\n\
+            Output: :- at(color, red, X), not at(drink, coffee, X).\n\
+            Input: It can't be that coffee is not being drunk at the red house.\n\
+            Output: :- at(color, red, X), not at(drink, coffee, X).\n\
+            Input: "
+
+prompt_w_context = contexto + prompt +"\nOutput: "
+
+
+
 
 def procesa_mensaje(msg):
 
     #print("Llega al programa python con mensaje: " + msg)
 
     # Petición a la LLM (Actualmente, modelo pequeño para probar)
-    payload = json.dumps({ "model": modelo, "prompt": prompt })
+    payload = json.dumps({ "model": modelo, "prompt": prompt_w_context })
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f"Bearer {AWANLLM_API_KEY}"
@@ -25,9 +52,12 @@ def procesa_mensaje(msg):
 
     # Envío al solver ASP
 
+    print("[Context]  " + contexto)
     print("[Prompt]  " +  prompt)
     print("[Answer]  " + json_res['choices'][0]['text'])
-    return "Placeholder para respuesta final"
+
+    #return "Placeholder para respuesta final"
+    return "Done"
 
 if __name__ == "__main__":
 
