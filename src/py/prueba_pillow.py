dad5884e9cb9d313f09f06b3664ad3f2a7a12237
PedroPazosCurra <pedropazos11@gmail.com>
2024-05-15 00:14:17 +0200

    Separadores para las casas en la representación de Einstein

--- src/py/prueba_pillow.py
+++ src/py/prueba_pillow.py
@@ -12,7 +12,24 @@ fondo = Image.open('..../resources/img/fondo_imagen_generada.png')
 # Crea objeto Draw
 dibujo = ImageDraw.Draw(fondo)
 
-def haz_casita(coordenadas, numero, persona, bebida, mascota, tabaco, color, tamaño):
+def dibuja_datos(coordenadas, tamaño, fuente, array_datos):
+
+    a, b = coordenadas
+    c = a + tamaño*2
+    d = b + tamaño*1.5
+
+    for i, dato in enumerate(array_datos):
+
+        # Escribe
+        dibujo.text(xy = (a + (c-a)/2 - tamaño*1.1, d + (d-b)*(0.75*i + 0.5)),
+                    text = dato.capitalize(),
+                    font= fuente,
+                    stroke_width= int(tamaño * 0.06),
+                    stroke_fill= 'black'
+                    )
+
+
+def representa_casa(coordenadas, numero, persona, bebida, mascota, tabaco, color, tamaño):
 
     # En base al nombre del color, una librería pasa un rgb. Si no puede, gris.
     try:
@@ -22,59 +39,33 @@ def haz_casita(coordenadas, numero, persona, bebida, mascota, tabaco, color, tam
 
     a, b = coordenadas
     c = a + tamaño*2
-    d = b + tamaño*1.5
+    d = b + tamaño*1.15
 
     # Dibuja la casa
-    dibujo.rectangle(xy = (a, b, c, d),
+    dibujo.polygon([(a - tamaño*0.3, b), (a,b), (a,d), (c,d), (c,b), (c + tamaño*0.3, b), ((c + a)/2, b - tamaño*1.3)],
                     fill = color_casa,
-                    outline = 'black')
-
-    dibujo.polygon([(a - tamaño*0.2, b), (c + tamaño*0.2, b), ((c + a)/2, b - tamaño*1.3)], fill = color_casa, outline = 'black')
+                    outline = 'black',
+                    width= int(tamaño * 0.1))
 
     # Crea elemento fuente
-    fuente = ImageFont.truetype("..../resources/fonts/OpenSans-Regular.ttf", size = tamaño*0.55)
+    fuente_numero = ImageFont.truetype("..../resources/fonts/OpenSans-Regular.ttf", size = tamaño*0.8)
     
     # Dibuja el número de la casa
-    dibujo.text(xy = (a + (c-a)/2 - tamaño*0.2, b + (d-b)/4),
+    dibujo.text(xy = (a + (c-a)/2 - tamaño*0.25, b - tamaño*0.2 ),
                 text = str(numero),
-                font= fuente,
-                stroke_width=2,
+                font= fuente_numero,
+                stroke_width= int(tamaño * 0.1),
                 stroke_fill='black'
                 )
     
-    # Escribe la persona
-    dibujo.text(xy = (a + (c-a)/2 - tamaño, d + (d-b)*0.5),
-                text = persona,
-                font= fuente,
-                stroke_width=2,
-                stroke_fill='black'
-                )
-    
-    # Escribe la mascota
-    dibujo.text(xy = (a + (c-a)/2 - tamaño, d + (d-b)),
-                text = mascota,
-                font= fuente,
-                stroke_width=2,
-                stroke_fill='black'
-                )
-    
-    # Escribe la bebida
-    dibujo.text(xy = (a + (c-a)/2 - tamaño, d + (d-b)*1.5),
-                text = bebida,
-                font= fuente,
-                stroke_width=2,
-                stroke_fill='black'
-                )
-    
-    # Escribe el tabaco
-    dibujo.text(xy = (a + (c-a)/2 - tamaño, d + (d-b)*2),
-                text = tabaco,
-                font= fuente,
-                stroke_width=2,
-                stroke_fill='black'
-                )
 
-def haz_casitas(casas):
+    fuente_letras = ImageFont.truetype("..../resources/fonts/OpenSans-Regular.ttf", size = tamaño*0.5)
+
+    # Escribe los datos debajo de la casa
+    dibuja_datos(coordenadas, tamaño, fuente_letras, [persona, bebida, mascota, tabaco])
+
+
+def representa_solucion(casas):
 
     num_casas = len(casas)
 
@@ -83,7 +74,11 @@ def haz_casitas(casas):
 
     for i, casa in enumerate(casas):
 
-        haz_casita(coordenadas= ((1000/(num_casas+1)) * (i + 1) - tamaño, 400),
+        # División de los 1000px que le toca a cada casa según cuántas casas hay
+        division = (1000/(num_casas+1)) * (i + 1)
+
+        # Dibuja la casa y escribe sus datos
+        representa_casa(coordenadas= (division - tamaño_casas, 400),
                     numero= casa["num"],
                     persona=casa["person"],
                     bebida=casa["beverage"],
@@ -91,6 +86,16 @@ def haz_casitas(casas):
                     tabaco=casa["tobacco"],
                     color= casa["color"],
                     tamaño= tamaño_casas)
+        
+        # Dibuja separador
+        dibujo.line(xy= [(division - tamaño_casas*1.25 - 1.068**tamaño_casas, 420), (division - tamaño_casas*1.25 - 1.068**tamaño_casas, 340 + 8*tamaño_casas)],
+                width= int(1.03**tamaño_casas),
+                fill= 'gray')
+        
+    # Dibuja último separador
+    dibujo.line(xy= [(((1000/(num_casas+1)) * (num_casas + 1)) - tamaño_casas*1.25 - 1.068**tamaño_casas, 420), (((1000/(num_casas+1)) * (num_casas + 1)) - tamaño_casas*1.25 - 1.068**tamaño_casas, 340 + 8*tamaño_casas)],
+            width= int(1.03**tamaño_casas),
+            fill= 'gray')
 
 casa1 = {
     "num": 1,
@@ -130,7 +135,7 @@ casa5 = {
     "color": "purple",
     "pet": "dog",
     "beverage": "milk",
-    "tobacco": "ducados"
+    "tobacco": "winston"
 }
 casa6 = {
     "num": 6,
@@ -138,7 +143,7 @@ casa6 = {
     "color": "pink",
     "pet": "dog",
     "beverage": "milk",
-    "tobacco": "ducados"
+    "tobacco": "bluem"
 }
 casa7 = {
     "num": 7,
@@ -174,9 +179,9 @@ casa10 = {
 }
 
 casas = [casa1, casa2, casa3]
-#casas = [casa1, casa2, casa3, casa4, casa5, casa6]
-#casas = [casa1, casa2, casa3, casa4, casa5, casa6, casa7, casa8, casa9, casa10]
-haz_casitas(casas)
+casas = [casa1, casa2, casa3, casa4, casa5, casa6]
+casas = [casa1, casa2, casa3, casa4, casa5, casa6, casa7, casa8, casa9, casa10]
+representa_solucion(casas)
 
 # Enseña la imagen por pantalla
 fondo.show()
